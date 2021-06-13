150c136f92faf2d422d0f23da4e3338afb7c24e1
Fomagran <fomagran6@naver.com>
2021-06-13 19:57:29 +0900

    저번에 만들었던 카드로 메인 꾸미기

--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,105 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String phoneNumber = "+82 10 2240 6112";
+
+  void changePhoneNumber() {
+    setState(() {
+      phoneNumber = "+82 10 1234 5678";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fomagran',
-      theme: ThemeData.dark(),
-      home: MyHomePage(title: 'Hello, I\'m Fomagran'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-                child: Text("Fomagran"),
-                onPressed: () {
-                  print("fomagran");
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black12,
-                ))
-          ],
-        ),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/profile.png'),
+          ),
+          Text(
+            "Fomagran",
+            style: TextStyle(
+                fontFamily: "Timmana",
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          Text("iOS & Flutter Developer",
+              style: TextStyle(
+                fontFamily: "Timmana",
+                fontSize: 20,
+                color: Colors.teal.shade100,
+              )),
+          SizedBox(
+            width: 150,
+            height: 20.0,
+            child: Divider(
+              color: Colors.teal.shade100,
+            ),
+          ),
+          Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 200),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "$phoneNumber",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: "Timmana",
+                        fontSize: 20),
+                  ))),
+          Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 200),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "fomagran6@naver.com",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: "Timmana",
+                        fontSize: 20),
+                  ))),
+          FlatButton(
+            onPressed: () {
+              changePhoneNumber();
+              print("$phoneNumber");
+            },
+            child: Card(
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 200),
+                child: ListTile(
+                    title: Center(
+                  child: Text(
+                    "Show more detail!",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Timmana",
+                        fontSize: 20),
+                  ),
+                ))),
+          )
+        ],
+      )),
+    ));
   }
 }
