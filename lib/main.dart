0b938a9c4dd8f0b5157c8c5561380e74231e6e6d
Fomagran <fomagran6@naver.com>
2021-06-13 14:34:42 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fomagran',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Hello, I\'m Fomagran'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                child: Text("Fomagran"),
+                onPressed: () {
+                  print("fomagran");
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black12,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
